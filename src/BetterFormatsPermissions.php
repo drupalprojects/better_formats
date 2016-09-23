5082f0c500eb46e1a754f070f051e39eb28c1b9c
Devin Carlson <devin@devincarlson.ca>
2016-09-23 10:53:23 -0400

    Refactored BetterFormatsPermissions to use dependency injection.

--- src/BetterFormatsPermissions.php
+++ src/BetterFormatsPermissions.php
@@ -2,32 +2,59 @@
 
 namespace Drupal\better_formats;
 
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Entity\EntityManagerInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 /**
- * Class BetterFormatsPermissions.
- *
- * @package Drupal\better_formats
+ * Provides dynamic permissions of the better formats module.
  */
-class BetterFormatsPermissions {
+class BetterFormatsPermissions implements ContainerInjectionInterface {
+
+  use StringTranslationTrait;
 
   /**
-   * Returns an array of entity type permissions.
+   * The entity manager.
    *
-   * Each entity type will show up as a separate row in the permissions UI,
-   * allowing controls for the display of format selection per entity type.
+   * @var \Drupal\Core\Entity\EntityManagerInterface
+   */
+  protected $entityManager;
+
+  /**
+   * Constructs a new BetterFormatsPermissions instance.
+   *
+   * @param \Drupal\Core\Entity\EntityManagerInterface $entity_manager
+   *   The entity manager.
+   */
+  public function __construct(EntityManagerInterface $entity_manager) {
+    $this->entityManager = $entity_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static($container->get('entity.manager'));
+  }
+
+  /**
+   * Returns an array of better formats permissions.
    *
    * @return array
-   *   The entity type permissions.
    */
   public function permissions() {
     $permissions = [];
 
-    foreach (\Drupal::entityManager()->getDefinitions() as $entity_type_id => $entity_type) {
+    // Generate permissions for each entity type.
+    foreach ($this->entityManager->getDefinitions() as $entity_type_id => $entity_type) {
       if ($entity_type->get('field_ui_base_route')) {
         $permissions['hide format selection for ' . $entity_type_id] = [
-          'title' => t('Hide format selection for @label', ['@label' => $entity_type->getLabel()]),
+          'title' => $this->t('Hide format selection for @label', ['@label' => $entity_type->getLabel()]),
         ];
       }
     }
+
     return $permissions;
   }
 
